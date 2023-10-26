9c108f60194fb6567a828e95a169b43fd1dbe6cd
Daniel Kloh <danielkloh06@gmail.com>
2023-10-26 16:32:25 -0300

    criando o projeto

---
+++ index.php
@@ -0,0 +1,26 @@
+<html lang='en'>
+
+<head>
+    <meta charset='utf-8' />
+    <link rel="stylesheet" href="style/custom.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet'>
+    <link href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css' rel='stylesheet'>
+
+</head>
+
+<body>
+    <div id='calendar' class="container mt-5 mb-5"></div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+    <script src='script/index.global.min.js'></script>
+    <script src="script/bootstrap5/index.global.min.js"></script>
+    <script src='script/core/locales-all.global.min.js'></script>
+    <script src="script/custom.js"></script>
+</body>
+
+</html>
